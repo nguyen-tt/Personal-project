f4f758380452f04d3ab650d2e98b3ba8ea378c12
Trung Nguyen <[trung.nguyenpro@hotmail.com](mailto:trung.nguyenpro@hotmail.com)>
2023-07-20 11:26:37 +0200

    button home done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <h1>home</h1>;
+  return (
+    <div className="home">
+      <div className="inside-home">
+        <h1>BIENVENUE</h1>
+        <Link to="/game" className="button-link">
+          <button type="button" className="button-home">
+            JOUER
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
