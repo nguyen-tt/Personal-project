50fb4948fc2c16206e91eaaf83f4d1555f041c20
Trung Nguyen <[trung.nguyenpro@hotmail.com](mailto:trung.nguyenpro@hotmail.com)>
2023-07-21 11:34:31 +0200

    added navbar on page notfound

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,14 +1,18 @@
 import notfound from "@assets/notfound.png";
 import footer from "@assets/footer.png";
+import NavBar from "@components/NavBar";
 
 function NotFound() {
   return (
     <div className="notfound">
       <div className="inside-notfound">
-        <img src={notfound} alt="not found" />
-        <div className="text-notfound">
-          <span>404</span>
-          <p>Not Found</p>
+        <NavBar />
+        <div className="inside-not">
+          <img src={notfound} alt="not found" />
+          <div className="text-notfound">
+            <span>404</span>
+            <p>Not Found</p>
+          </div>
         </div>
       </div>
       <div className="footer">
